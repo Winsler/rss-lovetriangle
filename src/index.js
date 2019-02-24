8cde895e2d13ea83cf71a1c76065f837102ebe31
Winsler <r.makretski@gmail.com>
2019-02-24 21:39:35 +0300

    decision

--- src/index.js
+++ src/index.js
@@ -3,5 +3,35 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	let trianglesIndexex = [],
+		triangles = 0;
+
+		function checkTriangle(index1, index2, index3) {
+			if (trianglesIndexex.indexOf(index1) == -1 &&
+				trianglesIndexex.indexOf(index2) == -1 &&
+				trianglesIndexex.indexOf(index3) == -1 &&
+				index1 >= 0 &&
+				index2 >= 0 &&
+				index3 >= 0 &&
+				index1 != preferences[index1] - 1) {
+
+				if (
+					(preferences[index1] - 1 == index2 && preferences[index2] - 1 == index3 && preferences[index3] - 1 == index1) ||
+					(preferences[index1] - 1 == index3 && preferences[index3] - 1 == index2 && preferences[index2] - 1 == index1)
+					) {
+					triangles++;
+					trianglesIndexex.push(index1);
+					trianglesIndexex.push(index2);
+					trianglesIndexex.push(index3);
+
+				};
+
+			};
+		};
+
+	preferences.forEach(function(lover, index) {
+			checkTriangle(index, lover - 1, preferences[lover - 1] - 1);
+	});
+
+	return triangles
 };
